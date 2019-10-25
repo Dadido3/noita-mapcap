ac2e0bff40bd3662c45446913192e1eb9278f564
David Vogel <Dadido3@aol.com>
2019-10-25 21:07:19 +0200

    Add travis for automatic releases

---
+++ scripts/pack_windows.x86.bat
@@ -0,0 +1,16 @@
+rd distribution /s/q
+
+mkdir distribution
+mkdir distribution/noita-mapcap
+
+robocopy "." "distribution/noita-mapcap/" init.lua LICENSE mod.xml README.md
+
+robocopy "data" "distribution/noita-mapcap/data" /e
+robocopy "files" "distribution/noita-mapcap/files" /e
+
+robocopy "bin/capture-b/" "distribution/noita-mapcap/bin/capture-b/" capture.dll README.md
+robocopy "bin/stitch/" "distribution/noita-mapcap/bin/stitch/" stitch.exe README.md
+
+cd distribution
+
+7z a -t7z Windows.x86.7z -m0=lzma2 -mx=9 -aoa noita-mapcap
