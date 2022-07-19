0ec2776705d5d4fd04a8fc9ad82db2e446203d77
David Vogel <Dadido3@aol.com>
2022-07-19 18:28:30 +0200

    Change coroutines to OnWorldPreUpdate callback

--- init.lua
+++ init.lua
@@ -9,13 +9,20 @@ function OnPlayerSpawned(player_entity)
 	--EntityLoad("mods/noita-mapcap/files/luacomponent.xml") -- ffi isn't accessible from inside lua components, scrap that idea
 	modGUI = GuiCreate()
 	GameSetCameraFree(true)
+
+	-- Start entity capturing right when the player spawn.
+	--DebugEntityCapture()
 end
 
-function OnWorldPostUpdate() -- this is called every time the game has finished updating the world
+-- Called *every* time the game is about to start updating the world
+function OnWorldPreUpdate()
 	wake_up_waiting_threads(1) -- Coroutines aren't run every frame in this sandbox, do it manually here.
 end
+
+-- Called *every* time the game has finished updating the world
+function OnWorldPostUpdate() end
 
 ModMagicNumbersFileAdd("mods/noita-mapcap/files/magic_numbers.xml") -- override some game constants
 
 -- Apply overrides.
-ModLuaFileAppend("data/scripts/perks/perk.lua", "mods/noita-mapcap/files/overrides/perks/perk.lua" )
+ModLuaFileAppend("data/scripts/perks/perk.lua", "mods/noita-mapcap/files/overrides/perks/perk.lua")
