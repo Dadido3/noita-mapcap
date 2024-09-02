3e5950810a0a72deaad93a7db68bb069cfd575cd
WUOTE <106106310+WUOTE@users.noreply.github.com>
2024-09-02 16:44:49 +0600

    Update modification.lua

--- files/modification.lua
+++ files/modification.lua
@@ -371,6 +371,13 @@ function Modification.SetMemoryOptions(memory)
 					ptr[0] = value -- This basically just changes the value that Noita forces to the "mods_have_been_active_during_this_run" member of the WorldStateComponent when any mod is enabled.
 				end,
 			},
+				{_Offset = 0x01007CA4, _BuildString = "Build Aug 12 2024 21:14:23", -- Steam build.
+				enableModDetection = function(value)
+					local ptr = ffi.cast("char*", 0x006B3925+6)
+					Memory.VirtualProtect(ptr, 1, Memory.PAGE_EXECUTE_READWRITE)
+					ptr[0] = value -- This basically just changes the value that Noita forces to the "mods_have_been_active_during_this_run" member of the WorldStateComponent when any mod is enabled.
+				end,
+			},
 			},
 		},
 	}
